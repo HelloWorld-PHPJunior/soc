22b8a0dc31f2f82553801d01a4826bdcfb536a9e
Dobriy-chelovek <divide.et.impera.dk@gmail.com>
2014-11-24 22:44:57 +0200

    Routing config

--- protected/config/main.php
+++ protected/config/main.php
@@ -38,7 +38,6 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -47,7 +46,6 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
         'db'=> require 'db.php',
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
