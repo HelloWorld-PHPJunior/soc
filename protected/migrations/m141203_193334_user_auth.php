c3d7a87be866809dc704be3ab72c285b29712223
lNoCommentsl <artem.kolotinskiy@gmail.com>
2014-12-03 21:44:06 +0200

    Merge branch 'master' of github.com:HelloWorld-PHPJunior/soc into group1-4
    Conflicts:
    	protected/views/layouts/default.haml

---
+++ protected/migrations/m141203_193334_user_auth.php
@@ -0,0 +1,25 @@
+<?php
+
+class m141203_193334_user_auth extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m141203_193334_user_auth does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
