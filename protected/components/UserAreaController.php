79e38bedb46498603ef305a6962f08d13b0e444a
danya <daniel22.198586@gmail.com>
2014-12-03 21:39:07 +0200

    rename user controller to auth

--- protected/components/UserAreaController.php
+++ protected/components/UserAreaController.php
@@ -11,7 +11,7 @@ class UserAreaController extends Controller
     public function init()
     {
         if(Yii::app()->user->isGuest){
-            $this->redirect(Yii::app()->createUrl('user/signin'));
+            $this->redirect(Yii::app()->createUrl('auth/signin'));
         }
     }
 } 
